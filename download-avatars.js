16087170e916c85660dd3e7e6976197e62698615
Danny K Pan <kaiwen006@gmail.com>
2018-01-22 19:46:37 +0000

    updated gitignore and implemented authentication in download-avatars to expand rate-limit

--- download-avatars.js
+++ download-avatars.js
@@ -1,16 +1,21 @@
 var request = require('request');
-
+var token = require('./secrets');
+var fs = require('fs');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': 'request',
+      'Authorization': 'token ' + token.GITHUB_TOKEN
     }
   };
-  request(url, function(err, res, body) {
-    cb(err, body);
+  request(options, function(err, res, body) {
+    if(err) {
+      throw err;
+    }
+    cb(err, res);
   });
   /*
     .on('error', function(err) {
@@ -30,3 +35,19 @@ getRepoContributors('jquery', 'jquery', function(err, result) {
   console.log('Result:', result);
 });
 
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function(err) {
+      throw err;
+    })
+    .on('response', function(response) {
+      console.log('Downloading avatar...');
+    })
+    .pipe(fs.createWriteStream(filePath))
+    .on('finish', function() {
+      console.log('Avatar download completed.');
+    });
+}
+
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./kvirani.jpg");
