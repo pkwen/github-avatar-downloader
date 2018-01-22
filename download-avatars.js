1e7e160806141239325765fa35a1fd1d15d1c8cd
Danny K Pan <kaiwen006@gmail.com>
2018-01-22 18:47:02 +0000

    added header

--- download-avatars.js
+++ download-avatars.js
@@ -2,3 +2,31 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
+  /*
+    .on('error', function(err) {
+      throw error;
+    })
+    .on('response', function(response) {
+      console.log(response);
+    })
+    .on('end', function() {
+      cb();
+      console.log('Task completed.');
+    });*/
+}
+
+getRepoContributors('jquery', 'jquery', function(err, result) {
+  console.log('Errors:', err);
+  console.log('Result:', result);
+});
+
